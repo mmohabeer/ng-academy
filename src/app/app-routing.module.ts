7b4e9453c4402ae27285e811d930c4e1a4fe628c
mmohabeer <mohabeer.mahessh@gmail.com>
2022-02-01 22:41:04 +0400

    app-07: Route protection with guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Router, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth/auth.guard";
 import { LoginComponent } from "./auth/login/login.component";
 import { PersonInputComponent } from "./person-input/person-input.component";
 import { PersonsComponent } from "./persons/persons.component";
 
 const routes: Routes = [
-  { path: '', component: PersonsComponent },
+  { path: '', component: PersonsComponent, canActivate: [AuthGuard] },
   { path: 'input', component: PersonInputComponent },
   { path: 'login', component: LoginComponent}
 ];
