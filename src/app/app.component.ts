ba5f72dd040fdce72bd8a75a9cfedecf68432c89
mmohabeer <mohabeer.mahessh@gmail.com>
2022-02-01 20:35:39 +0400

    app-01 : Directives and string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-web-academy';
+
+  persons: string[] = ['Donald','Mickey','Teacher'];
+
 }
---
+++ src/app/persons/persons.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persons',
+  templateUrl: './persons.component.html',
+  styleUrls: ['./persons.component.css']
+})
+export class PersonsComponent implements OnInit {
+
+  @Input()
+  personList!: string[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
